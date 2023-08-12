b8740c05e60dae10b6bb5da6c19ca2bbbfbe651d
Saujana Shafi <saujanashafi2003@gmail.com>
2023-08-12 09:38:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function myFunction(){
-    alert("Hey digipeeps!!!")
+function myFunction() {
+  alert("Hey digipeeps!!!");
+
+  console.log("Hey digipeeps!!!");
+
+  const footer = document.getElementById("footer");
+
+  footer.style.backgroundImage =
+    "url('https://o-cdf.sirclocdn.com/unsafe/o-cdn-cas.sirclocdn.com/parenting/images/park-joo-hyun-acara-tv.width-800.format-webp.webp')";
 }
